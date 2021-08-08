4ba56db3e4fac3bc0f5930c8cbf2490e27f5a754
EMcKague <evan.mckague@gmail.com>
2021-08-08 12:42:54 -0600

    Added some styling to article list

--- TodayILearned/TodayILearned/urls.py
+++ TodayILearned/TodayILearned/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
@@ -10,3 +11,6 @@ urlpatterns = [
     path('about/', views.aboutPage),
     path('', views.homePage),
 ]
+
+# add the staticfiles to the url paths
+urlpatterns += staticfiles_urlpatterns()
