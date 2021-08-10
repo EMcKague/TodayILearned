99f86b41e1ae9012a4f71533ad30a842512b12f3
EMcKague <evan.mckague@gmail.com>
2021-08-09 19:47:08 -0600

    created article creation page and required login

--- TodayILearned/articles/views.py
+++ TodayILearned/articles/views.py
@@ -1,8 +1,9 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Article
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from . import forms
 
 # Create your views here.
 
@@ -23,4 +24,10 @@ def article_detail(request, slug):
 
 @login_required(login_url='/accounts/login/')
 def article_create(request):
-    return render(request, 'articles/article_create.html')
+    if request.method == 'POST':
+        form = forms.CreateArticle(request.POST, request.FILES)
+        if form.is_valid():
+            return redirect('articles:listView')
+    else:
+        form = forms.CreateArticle()
+    return render(request, 'articles/article_create.html', {'form': form})
