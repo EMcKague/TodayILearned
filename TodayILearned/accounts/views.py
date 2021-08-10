4415535f0dde63d8cb5c5b2eb11e2bdce2e9e088
EMcKague <evan.mckague@gmail.com>
2021-08-09 19:14:31 -0600

    created login & create page. Added logout button

--- TodayILearned/accounts/views.py
+++ TodayILearned/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, logout
 
 # Create your views here.
 
@@ -8,7 +9,8 @@ def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request, user)
             return redirect('articles:listView')
 
     else:
@@ -20,8 +22,18 @@ def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            #log in user
-            return redirect('articles:listView')
+            user = form.get_user()
+            login(request, user)
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+                return redirect('articles:listView')
     else:
         form = AuthenticationForm()
     return render(request, 'accounts/login.html', {'form': form})
+
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('articles:listView')
